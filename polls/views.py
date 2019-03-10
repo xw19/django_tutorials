2d982cf51185b322f8ec1b41c1c1922ac394409a
Sourav Moitra <sourav.moitr@gmail.com>
2019-03-10 13:53:46 +0530

    First commit

---
+++ polls/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+from .models import Question
+
+def index(request):
+    # return HttpResponse("Hello, world. You are at the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s" % question_id)
+
+
+def results(request, question_id):
+    response = "You are looking at the results of question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
